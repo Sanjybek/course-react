db2d4b2279cd57b2ec0dfa7ac0c45e1b823fbf3e
Sanjybek <sanjybek003@gmail.com>
2023-11-28 16:27:13 +0600

    lesson 8

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,31 @@ import List from "./components/List/List"
 import UserList from "./components/UserList/UserList"
 import TodoList from "./components/TodoList/TodoList";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Header</div>,
+
+  },
+  {
+    path: "/todo-list",
+    element: <TodoList/>,
+    
+  },
+  {
+    path: "/user-list",
+    element: <UserList/>,
+  },
+]);
+
 function App() {
-
-  const [count, setCount] = useState(1)
-  // console.log(count);
-  const handleCount = () => {
-    setCount(2)
-  }
-  // const arr = ['item1','item2','item3']
-  // const [a, b] = arr
-  // console.log(a,b, 'кортедж');
-
-
-  // const count = 1
-  const str = 'l9'
-  const arr = ['item1','item2','item3']
-  const numer = 123456
-
-  
-  return (
-      <>
+  return <RouterProvider router={router} />
+     
           {/* <UserList/> */}
           {/* <h1 className={'list'}>TODO LIST</h1>
           <button onClick={handleCount}>click</button>
@@ -37,10 +42,9 @@ function App() {
           arr={arr}
           numer={numer}
           /> */}
-          <TodoList/>
+          // <TodoList/>
         
-      </>
-  )
+  
 
 }
 
