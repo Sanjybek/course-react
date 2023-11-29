111aea0503e88613d7a4becdb4d31702984d9f5a
Sanjybek <sanjybek003@gmail.com>
2023-11-29 16:30:41 +0600

    error branch

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import  router  from "./pages/routers";
 import { useState } from "react";
 import styles from "./App.module.css"
 
-import Addtask from "./components/AddTask/AddTask";
-import List from "./components/List/List";
+// import Addtask from "./components/AddTask/AddTask";
+// import List from "./components/List/List";
 import Posts from "./components/Posts/Posts";
 import Count from "./components/Count/Count";
 
@@ -42,57 +42,57 @@ import {
 function App() {
   return <RouterProvider router={router} />
 
-  const [count, setCount] = useState(1)
-  // console.log(count);
-  const handleCount = () => {
-    setCount(2)
-  }
+  // const [count, setCount] = useState(1)
+  // // console.log(count);
+  // const handleCount = () => {
+  //   setCount(2)
+  // }
+  // // const arr = ['item1','item2','item3']
+  // // const [a, b] = arr
+  // // console.log(a,b, 'кортедж');
+
+
+  // // const count = 1
+  // const str = 'l9'
   // const arr = ['item1','item2','item3']
-  // const [a, b] = arr
-  // console.log(a,b, 'кортедж');
-
-
-  // const count = 1
-  const str = 'l9'
-  const arr = ['item1','item2','item3']
-  const numer = 123456
+  // const numer = 123456
 
 
   
 
   
-  return (
-      <>
-        {/* <h1 className={'list'}>TODO LIST</h1>
-        <h1 className={'list'}>TODO LIST</h1>
-        <Count/>
-        <button onClick={handleCount}>click</button>
-        <Addtask></Addtask>
-        <List
-        count = {count}
-        str={str}
-        arr={arr}
-        numer={numer}
-        /> */}
-        <Posts/>
-        />
-          {/* <UserList/> */}
-          {/* <h1 className={'list'}>TODO LIST</h1>
-          <button onClick={handleCount}>click</button>
-          <Addtask></Addtask>
-          <List
-          count = {count}
-          str={str}
-          arr={arr}
-          numer={numer}
-          /> */}
-          <TodoList/>
+  // return (
+  //     <>
+  //       {/* <h1 className={'list'}>TODO LIST</h1>
+  //       <h1 className={'list'}>TODO LIST</h1>
+  //       <Count/>
+  //       <button onClick={handleCount}>click</button>
+  //       <Addtask></Addtask>
+  //       <List
+  //       count = {count}
+  //       str={str}
+  //       arr={arr}
+  //       numer={numer}
+  //       /> */}
+  //       <Posts/>
+  //       />
+  //         {/* <UserList/> */}
+  //         {/* <h1 className={'list'}>TODO LIST</h1>
+  //         <button onClick={handleCount}>click</button>
+  //         <Addtask></Addtask>
+  //         <List
+  //         count = {count}
+  //         str={str}
+  //         arr={arr}
+  //         numer={numer}
+  //         /> */}
+  //         <TodoList/>
 
           
 
         
-      </>
-  )
+      // </>
+  // )
 
 }
 
--- src/pages/routers.js
+++ src/pages/routers.js
@@ -2,10 +2,11 @@ import React from 'react'
 import styles from "../App.module.css"
 
 import Addtask from "../components/AddTask/AddTask";
-import List from "../components/List/List"
+// import List from "../components/List/List"
 import UserList from "../components/UserList/UserList"
 import TodoList from "../components/TodoList/TodoList";
-
+import Posts from "../components/Posts/Posts";
+import Count from "../components/Count/Count";
 import {createBrowserRouter} from "react-router-dom";
 import TodoLIstPage from './TodoListPage/TodoLIstPage';
 import Header from '../components/Header/Header';
