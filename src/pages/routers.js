b2dfa8470a6b031aad33fb45e5005232e5610e8b
Sanjybek <sanjybek003@gmail.com>
2023-11-30 15:27:14 +0600

    dev

--- src/pages/routers.js
+++ src/pages/routers.js
@@ -1,72 +1,15 @@
 import React from 'react'
-import styles from "../App.module.css"
 
-import Addtask from "../components/AddTask/AddTask";
-import Posts from "../components/Posts/Posts";
-import Count from "../components/Count/Count";
-import UserList from "../components/UserList/UserList"
+
 import {createBrowserRouter} from "react-router-dom";
-import TodoLIstPage from './TodoListPage/TodoListPage';
-import Header from '../components/Header/Header';
+import PostsPage from './PostPage';
+
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <>
-          <Header/>
-          <div>Header</div>
-        </>
-      )
-  
-    },
-    {
-      path: "/todo-list",
-      element: (
-        <>
-          <Header/>
-          <TodoLIstPage/>
-          {/* <TodoList/> */}
-        </>
-      )
-      
-    },
-    {
-      path: "/user-list",
-      element: (
-        <>
-          <Header/>
-          <UserList/>,
-        </>
-      )
-    },
-    {
-      path: "/add-task",
-      element: (
-        <>
-          <Header/>
-          <Addtask/>
-        </>
-      )
-    },
-    {
-      path: "/count",
-      element: (
-        <>
-          <Header/>
-          <Count/>
-        </>
-      )
-    },
-    {
-      path: "/posts",
-      element: (
-        <>
-          <Header/>
-          <Posts/>
-        </>
-      )
-    },
+      element: <PostsPage/>
+    }
   ]);
 export default router 
